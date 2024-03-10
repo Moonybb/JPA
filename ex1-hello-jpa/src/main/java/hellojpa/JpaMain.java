9fc560a706b8f1feda43bfd05ac48847b6053506
Youngmoon Oh <moony_bb@naver.com>
2024-03-10 16:45:38 +0900

    값 타입 컬렉션

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,9 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.List;
+import java.util.Set;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -18,9 +21,33 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUserName("hello");
-            member.setHomeAdress(new Adress("city", "street", "10000"));
-            member.setWorkPeriod(new Period());
+            member.setHomeAdress(new Adress("home", "street", "10000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAdressHistory().add(new Adress("old1", "street", "10000"));
+            member.getAdressHistory().add(new Adress("old2", "street", "10000"));
+
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("=============START=============");
+            Member findMember = em.find(Member.class, member.getId());
+
+            // 값 타입 컬렉션도 지연 로딩 전략 사용
+            List<Adress> adressHistory = findMember.getAdressHistory();
+            for(Adress adress : adressHistory) {
+                System.out.println("adress = " + adress.getCity());
+            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFood = " + favoriteFood);
+            }
 
             tx.commit();
 
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,34 +1,40 @@
 package hellojpa;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 @NoArgsConstructor
 @AllArgsConstructor
-public @Getter @Setter class Member extends BaseEntity {
-    @Id @GeneratedValue
+public @Getter
+@Setter class Member extends BaseEntity {
+    @Id
+    @GeneratedValue
     @Column(name = "memberId")
     private Long id;
 
     @Column(name = "USERNAME")
     private String userName;
 
-    // 기간 Period
-    @Embedded
-    private Period workPeriod;
-//    private LocalDateTime startDate;
-//    private LocalDateTime endDate;
-
-    // 주소 Adress
     @Embedded
     private Adress homeAdress;
-//    private String city;
-//    private String street;
-//    private String zipCode;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Adress> adressHistory = new ArrayList<>();
 
 }
