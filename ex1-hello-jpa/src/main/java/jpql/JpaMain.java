3114caf0c005684fdf7862c45ba0c15776fe401a
Youngmoon Oh <moony_bb@naver.com>
2024-03-10 20:29:46 +0900

    객체지향 쿼리 언어1 - 기본 문법 : 서브쿼리

--- ex1-hello-jpa/src/main/java/jpql/JpaMain.java
+++ ex1-hello-jpa/src/main/java/jpql/JpaMain.java
@@ -20,35 +20,18 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member");
             member.setAge(10);
-            member.setTeam(team);
+            member.changeTeam(team);
             em.persist(member);
 
 
             em.flush();
             em.clear();
 
-            // 내부 조인
-            String innerJoinQuery = "select m from Member m inner join m.team t";
-            List<Member> members1 = em.createQuery(innerJoinQuery, Member.class)
-                    .getResultList();
+            // JPA는 where, having절에서만 서브 쿼리 사용 가능
+            // select 절도 가능 (하이버네이트에서 지원)
+            // from 절의 서브 쿼리는 현재 JPQL에서 불가능 : 조인으로 풀 수 있으면 풀어서 해결
+            String query = "select (select avg(m1.age) from Member m1) from Member m";
 
-            // 외부 조인
-            String leftJoinQuery = "select m from Member m left join m.team t";
-            List<Member> members2 = em.createQuery(leftJoinQuery, Member.class)
-                    .getResultList();
-
-            // 세타 조인
-            String cetaJoinQuery = "select m from Member m, Team t where m.username = t.name";
-            List<Member> members3 = em.createQuery(cetaJoinQuery, Member.class)
-                    .getResultList();
-
-            // 조인 대상 필터링
-            // ex) 회원과 팀을 조인하면서 회원과 팀의 이름이 같은 대상 외부 조인
-            // JPQL : select m, t from Member m left join Team t on m.username = t.name
-            // SQL : select m.*, t.* from Member m left join Team t on m.username = t.name
-            String query = "select m from Member m left join m.team t on m.username = t.name";
-            List<Member> members4 = em.createQuery(query, Member.class)
-                    .getResultList();
 
             tx.commit();
         }catch (Exception e) {
